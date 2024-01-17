0894a33280fde1571883b5914bf838041ae1e3e4
Melvin Junior Morales Lovera <146649655+MelvinjmlUCAB@users.noreply.github.com>
2024-01-17 18:11:32 +0000

    se agrego archivo hola mundo

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mi Página</title>
+    </head>
+    <body>
+        <script>
+            document.write("¡Hola Mundo!");
+
+
+        </script>
+
+
+
+
+
+    </body>
+</html>
